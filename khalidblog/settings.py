9b3f63f85e87c97eeb4b6b04536513bd00de6315
Abubakar Zorrain <abubakarzorrain71@gmail.com>
2021-05-13 03:11:23 +0500

    Initial commit

--- khalidblog/settings.py
+++ khalidblog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
